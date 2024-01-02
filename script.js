ca974919aa0e025eda44353d00c6e6e0295432fe
Izaan Khan <izaankhan.x@gmail.com>
2024-01-03 02:36:26 +0500

    new preloader

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ window.addEventListener('load', function() {
     const loader = document.querySelector('#preloader');
     setTimeout(function() {
       loader.style.display = 'none';
-    }, 2000);
+    }, 3000);
   });
   
   
